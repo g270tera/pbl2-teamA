dc616d97be0fbf8b1c9aa3d5bb992bed6080ab21
g270tera <g520270b@mails.cc.ehime-u.ac.jp>
2022-01-19 12:15:40 +0900

    test

---
+++ pbl2/logintop.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <title>You 柔不断</title>
+ <style type="text/css">
+ .title{
+ color: white;
+ font-size: 50px;
+ text-align: center;
+ margin-top: 100px;
+ }
+ .subtitle{
+ color: white;
+ font-size: 25px;
+ text-align: center;
+ margin-bottom: 70px;
+ }
+ .logbtn, .newac{
+ display: flex;
+ font-size: 30px;
+ margin:15px auto;
+ cursor: pointer;
+ border-color: black;
+ border: 2px;
+ border-radius: 5px;
+ background-color: white;
+ color: #669933;
+ width: 200px;
+ justify-content: center;
+ -webkit-box-sizing: border-box;
+ -moz-box-sizing: border-box;
+ box-sizing: border-box;
+ -webkit-transition: all .3s;
+ transition: all .3s;
+ }
+ .logbtn:hover, .newac:hover{
+ background-color: white;
+ color: black;
+ }
+ </style>
+</head>
+<body bgcolor="669933">
+ <div class="logtop">
+ <p><div class="title">You 柔不断</div>
+ <div class="subtitle">～今日は何する？～</div>
+ <p><button class="logbtn", id="logbtn", onclick="location.href='./login.php'">ログイン</button>
+ </p>
+ <p><button class="newac", id="newac", onclick="location.href='./join'"><ruby> 新 規 作 成 <rt> し ん き さ く せ い
+</rt></ruby></button>
+ </p>
+ </p>
+ </div>
+</body>
+</html>
